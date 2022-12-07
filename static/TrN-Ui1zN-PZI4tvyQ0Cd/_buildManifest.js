9d190642b764de86447c771e4ef9377698f0580e
Bogdan <b@bmazur.ru>
2022-12-07 22:42:57 +0300

    Updates

---
+++ static/TrN-Ui1zN-PZI4tvyQ0Cd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e229078c6015247c.css","static/chunks/pages/index-b68c70760573b014.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
