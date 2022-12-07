621d29ce4a3edd787ed1c095e1e885c077257be6
Bogdan <b@bmazur.ru>
2022-12-07 08:11:50 +0300

    feat: next

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Logo.module.css';
+
+export default function Logo() {
+  return (
+    <h1 className={styles.title}>
+      <span className={styles.title_background}>
+        <span className={[styles.title_foreground, styles.animated].join(' ')}>
+          [ntlstl]
+        </span>
+      </span>
+    </h1>
+  );
+}
