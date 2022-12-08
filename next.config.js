a0a18229108619b0682332d1747bbdf3e1e46e86
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2022-12-08 08:11:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'ntlstl'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
