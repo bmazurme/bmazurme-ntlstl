6b3a1827f0416223a3006d65dbfda64f1f40b306
Bogdan <b@bmazur.ru>
2022-12-08 08:27:35 +0300

    fix: conf

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'ntlstl'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
 }
 
 module.exports = nextConfig
