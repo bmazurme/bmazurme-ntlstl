ee6b8374941d66903260f2df5f5401094c9f8b91
Bogdan <b@bmazur.ru>
2022-12-09 09:56:11 +0300

    fix: menu

---
+++ src/components/Menu/mockData.ts
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: 'LinkedIn',
+    url: '#',
+  },
+  {
+    label: 'GitHub',
+    url: '#',
+  },
+  {
+    label: 'E-mail',
+    url: '#',
+  },
+];
+
+export default links;
