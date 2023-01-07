616c6c5ef0609ea876c657f66080c07b6aeb292e
Bogdan <b@bmazur.ru>
2023-01-07 12:01:24 +0300

    fix: img

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,7 +9,7 @@ export default function About({ info }: { info: IProfile}) {
 
   return (
     <section className={styles.about}>
-      <img className={styles.image}></img>
+      <img className={styles.image} src="https://avatars.githubusercontent.com/u/49879328?s=400&v=4" />
       <div className={styles.text}>
         <h1 className={styles.name}>{info.name}</h1>
         <h3 className={styles.profession}>{info.profession}, {getAge(info.age)}</h3>
