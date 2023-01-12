bec9df93391d51375e4b5c647bcca7bcf02d2b5c
Bogdan <b@bmazur.ru>
2023-01-12 15:29:59 +0300

    fix: img

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -22,7 +22,7 @@ export default function Content({ toggleLanguage, language, children }: IProps) 
         <meta property="og:site_name" content="ntlstl" key="ogsitename" />
         <meta property="og:url" content="/profile"  key="ogurl" />
         <meta property="og:description" content="ntlstl" key="ogdesc" />
-        <meta property="og:image" content={short.src} key="ogimage" />
+        <meta property="og:image" content="https://main--celebrated-pithivier-e91356.netlify.app/_next/static/media/sh-screen.371a3eee.png" key="ogimage" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header toggleLanguage={toggleLanguage} language={language} />
