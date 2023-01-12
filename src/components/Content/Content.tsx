a6456e3e11fc85360e85a33eba06783a9f27c7d9
Bogdan <b@bmazur.ru>
2023-01-12 14:58:31 +0300

    fix: meta

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { Header, Footer } from '..';
 import ILang from '../../interfaces/ILang';
 import styles from './Content.module.css';
+import short from '../../../img/short.png';
 
 interface IProps {
   toggleLanguage: (label: string) => void;
@@ -17,6 +18,11 @@ export default function Content({ toggleLanguage, language, children }: IProps) 
       <Head>
         <title>ntlstl | web developer</title>
         <meta name="description" content="ntlstl" />
+        <meta property="og:title" content="Bogdan" />
+        <meta property="og:site_name" content="ntlstl" />
+        <meta property="og:url" content="/profile" />
+        <meta property="og:description" content="ntlstl" />
+        <meta property="og:image" content={short.src} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header toggleLanguage={toggleLanguage} language={language} />
