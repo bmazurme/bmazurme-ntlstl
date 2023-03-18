888003bce10466374ef878de513dda1ca592123e
Bogdan <b@bmazur.ru>
2023-03-18 20:35:17 +0300

    fix: data

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -3,9 +3,9 @@ import { IProfile } from '../../interfaces';
 
 export default function Tech({ info }: { info: IProfile}) {
   const techList = [
-    'JavaScript', 'TypeScript', 'React.js', 'Vue.js', 'Next.js', 'Express.js', 'Redux', 'RTK', 'Jest',
+    'JavaScript', 'TypeScript', 'React.js', 'Vue.js', 'Next.js', 'Nest.js', 'Express.js', 'Redux', 'RTK', 'Jest',
     'Webpack', 'Parcel', 'PostCSS', 'Eslint', 'MUI', 'Tailwind', 'BEM', 'Canvas', 'MongoDB', 'PostgreSQL',
-    'Git', 'Docker', 'CSR', 'SSR', 'REST API', 'Axios', 'Figma', 'GitHub Actions'
+    'Git', 'Docker', 'CSR', 'SSR', 'REST', 'GraphQL', 'Axios', 'Figma', 'GitHub Actions', 'Sequelize', 'Mongoose'
   ];
 
   return (
