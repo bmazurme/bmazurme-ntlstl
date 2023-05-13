d4a66d828640fcfb8e394336cd41cf60c6729013
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-05-14 00:35:32 +0300

    fix: tech

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -4,6 +4,7 @@ import { IProfile } from '../../interfaces';
 export default function Tech({ info }: { info: IProfile}) {
   const techList = [
     'JavaScript', 'TypeScript', 'React.js', 'Vue.js', 'Next.js', 'Nest.js', 'Express.js', 'Redux', 'RTK', 'React-d3-tree',
+    'React-dnd',
     'Jest', 'Webpack', 'Parcel', 'PostCSS', 'Eslint', 'MUI', 'Tailwind', 'BEM', 'Canvas', 'MongoDB', 'PostgreSQL',
     'Git', 'Docker', 'CSR', 'SSR', 'REST', 'GraphQL', 'Axios', 'Figma', 'GitHub Actions', 'Sequelize', 'Mongoose'
   ];
