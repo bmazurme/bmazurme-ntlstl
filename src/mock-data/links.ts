a603beb5dacde79e34c72afe99e1a8da3fee9fb8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-06-18 11:27:04 +0300

    fix: links

--- src/mock-data/links.ts
+++ src/mock-data/links.ts
@@ -1,11 +1,11 @@
 import { Links } from '../utils';
 
 const links = [
-  { label: 'LinkedIn', url: 'https://www.linkedin.com/in/bogdan-mazur-aba74287' },
-  { label: 'GitHub', url: 'https://github.com/bmazurme' },
+  { label: 'LinkedIn', url: Links.LINKEDIN },
+  { label: 'GitHub', url: Links.GITHUB },
   { label: 'CV', url: Links.PROFILE },
   { label: 'Portfolio', url: Links.PORTFOLIO },
-  { label: 'E-mail', url: 'mailto:ntlstlwk@gmail.com' },
+  { label: 'E-mail', url: Links.EMAIL },
 ];
 
 export default links;
