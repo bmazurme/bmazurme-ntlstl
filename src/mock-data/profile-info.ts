958ca4663eb9f9cd5efcf0b428fe456c1b11730f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-06-18 11:26:41 +0300

    fix: data

--- src/mock-data/profile-info.ts
+++ src/mock-data/profile-info.ts
@@ -256,7 +256,7 @@ const profileInfo: Record<string, TypeProfile> = {
         from: '2022',
         to: 'по настоящее время',
         description: 'Разработка новых инструментов CRM-системы с использование фреймворка Vue.js. Разработка UI-компонентов, REST API, адаптивная-верстка, написание unit-тестов. Исправление ошибок.',
-        stack: 'Stack: Vue.js, React.js, Next.js, Express.js, Redux, RTK, React-de-tree, React-dnd, JS/TS, MUI, Tailwind, CSS, SASS, Jest, Webpack, Docker, Maven, GitLab.',
+        stack: 'Stack: Vue.js, React.js, Next.js, Express.js, Redux, RTK, React-d3-tree, React-dnd, JS/TS, MUI, Tailwind, CSS, SASS, Jest, Webpack, Docker, Maven, GitLab.',
       },
     ],
     portfolioList: [
