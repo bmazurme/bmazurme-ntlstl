f2cb8a275b0621b5cc3d71193e12f8879c1a7a0a
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-05 16:22:40 +0300

    fix: tech-list

--- src/mock-data/tech-list.ts
+++ src/mock-data/tech-list.ts
@@ -1,7 +1,7 @@
 const techList = [
   'JavaScript', 'TypeScript', 'React.js', 'Vue.js', 'Next.js', 'Nest.js', 'Express.js', 'Redux', 'Redux Thunk',
-  'RTK', 'React-d3-tree', 'React-dnd', 'Cypress', 'Jest', 'Webpack', 'Parcel', 'PostCSS', 'Eslint', 'MUI',
-  'Tailwind', 'BEM', 'Canvas', 'MongoDB', 'PostgreSQL', 'Git', 'Docker', 'CSR', 'SSR', 'REST', 'GraphQL',
+  'RTK', 'React-d3-tree', 'Recharts', 'React-dnd', 'Cypress', 'Jest', 'Webpack', 'Parcel', 'PostCSS', 'Eslint',
+  'MUI', 'Tailwind', 'BEM', 'Canvas', 'MongoDB', 'PostgreSQL', 'Git', 'Docker', 'CSR', 'SSR', 'REST', 'GraphQL',
   'Axios', 'Figma', 'GitHub Actions', 'Sequelize', 'Mongoose',
 ];
 
