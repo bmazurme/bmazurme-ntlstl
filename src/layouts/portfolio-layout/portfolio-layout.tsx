fb82da3ae9d63a81b677c39aa6a0d8ef5997acdf
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:21:42 +0300

    fix: portfolio-layout

---
+++ src/layouts/portfolio-layout/portfolio-layout.tsx
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import ProjectCard from '../../components/project-card';
+
+import { useAppContext } from '../../context/app-context';
+
+import styles from './portfolio-layout.module.css';
+
+export default function Portfolio() {
+  const context = useAppContext();
+  const { info } = context as { info: TypeProfile };
+
+  return (
+    <section className={styles.portfolio}>
+      <h2 className={styles.title}>{info.portfolio}</h2>
+      <ul className={styles.cards}>
+        {info.portfolioList.map((card: TypeCard) => <ProjectCard key={uuidv4()} {...card} />)}
+      </ul>
+    </section>
+  );
+}
