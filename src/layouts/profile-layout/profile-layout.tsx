066bb1eab0581b4fe0d9566232dd012e8483dda5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:21:17 +0300

    fix: profile-layout

---
+++ src/layouts/profile-layout/profile-layout.tsx
@@ -0,0 +1,15 @@
+import AboutSection from '../../components/about-section';
+import TechSection from '../../components/tech-section';
+import EducationSection from '../../components/education-section';
+import CompanySection from '../../components/company-section';
+
+export default function ProfileLayout() {
+  return (
+    <main>
+      <AboutSection />
+      <TechSection />
+      <EducationSection />
+      <CompanySection />
+    </main>
+  );
+}
