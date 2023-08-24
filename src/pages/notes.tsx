d8966b7bf4a276248aa28518b8161353de19782e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-24 08:20:42 +0300

    fix: notes

---
+++ src/pages/notes.tsx
@@ -0,0 +1,11 @@
+import Content from '../components/core/content';
+import NotesLayout from '../layouts/ notes-layout';
+// import ProfileLayer from '../layers/profile';
+
+export default function Profile() {
+  return (
+    <Content>
+      <NotesLayout />
+    </Content>
+  );
+}
