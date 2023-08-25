1eaf285053e92a28284cd860ce71e52af441e3d9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-25 08:21:38 +0300

    fix: portfolio

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,4 +1,4 @@
-import Content from '../components/core/content';
+import Content from '../components/content';
 import PortfolioLayer from '../layouts/portfolio-layout';
 
 export default function Profile() {
