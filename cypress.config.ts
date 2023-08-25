a89066400310613b22861710670604bd225d8df4
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-26 00:26:17 +0300

    feat: cypress

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
