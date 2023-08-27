cc871b61832d8d99de64ded4226ef996fc8ae658
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-27 11:19:29 +0300

    fix: app-context

--- src/context/app-context.tsx
+++ src/context/app-context.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/jsx-no-constructed-context-values */
 import {
   ReactNode, createContext, useContext, useState, useCallback,
