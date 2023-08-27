981d13c3451102816f9dce0ddaccf1a8b6a08268
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-27 11:18:36 +0300

    feat: 404-page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import NotFoundLayout from '../layouts/not-found-layout';
+
+import styles from '../styles/home.module.css';
+
+export default function NotFoundPage() {
+  return (
+    <div className={styles.container}>
+      <NotFoundLayout />
+    </div>
+  );
+}
