3d285282281c1c22c8b9abf90098165ca04c93e8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-27 11:20:18 +0300

    fix: components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,8 @@
 import Logo from './logo';
 import Header from './header';
 import Footer from './footer';
+import NotFound from './not-found';
 
-export { Logo, Header, Footer };
+export {
+  Logo, Header, Footer, NotFound,
+};
