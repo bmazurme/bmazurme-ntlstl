62afe599e9a8e48a3a75d8ec9d8e222f5a0f1c13
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 08:18:03 +0300

    fix: notes-layout

--- src/layouts/notes-layout/notes-layout.tsx
+++ src/layouts/notes-layout/notes-layout.tsx
@@ -1,10 +1,16 @@
+import { useEffect, useState } from 'react';
+
 import MenuSecondary from '../../components/menu-secondary';
 import Cards from './componets/cards';
 
+import { TypeNoteCard } from '../note-layout';
+
 import style from './notes-layout.module.css';
 
-export default function NotesLayout({ links, cards }
-  : { links: { name: string; to: string; }[]; cards: string[]; }) {
+export default function NotesLayout({ links, cards }: {
+  links: { name: string; to: string; }[];
+  cards: TypeNoteCard[];
+}) {
   return (
     <div className={style.notes}>
       <MenuSecondary links={links} />
