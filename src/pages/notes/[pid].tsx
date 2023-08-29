93584eb773a44795d38ba82e02094ba36be4e938
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 08:16:50 +0300

    fix: notes

--- src/pages/notes/[pid].tsx
+++ src/pages/notes/[pid].tsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+
 import Content from '../../components/content';
 import NotesLayout from '../../layouts/notes-layout';
-
 import { noticeLinks } from '../../mock-data/links';
-import { cards } from '../../mock-data/cards';
 
 export default function Test() {
   const router = useRouter();
   const { pid } = router.query;
-  const index = noticeLinks.findIndex((x) => (x.to.replace('/', '') === pid));
-  const getCards = pid !== '/' ? [cards[index]] : cards;
+  const url = '/api/cards';
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const getCards = async () => {
+      try {
+        const response = await fetch(`${url}/${pid}`);
+
+        if (response.ok) {
+          const result = await response.json();
+          setCards(result.cards);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCards();
+  }, [pid]);
 
   return (
     <Content>
-      <NotesLayout links={noticeLinks} cards={getCards} />
+      <NotesLayout links={noticeLinks} cards={cards} />
     </Content>
   );
 }
--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
+
 import Content from '../../components/content';
 import NotesLayout from '../../layouts/notes-layout';
 
 import { noticeLinks } from '../../mock-data/links';
-import { cards } from '../../mock-data/cards';
 
 export default function Profile() {
+  const url = '/api/cards';
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const getCards = async () => {
+      try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+          const result = await response.json();
+          setCards(result.cards);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCards();
+  }, []);
+
   return (
     <Content>
       <NotesLayout links={noticeLinks} cards={cards} />
