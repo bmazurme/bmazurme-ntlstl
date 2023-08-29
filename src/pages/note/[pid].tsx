0fe32d17fde36d5d8d75c3a496470cf373f984f8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-08-29 08:17:03 +0300

    fix: note

--- src/pages/note/[pid].tsx
+++ src/pages/note/[pid].tsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+
 import Content from '../../components/content';
+import NoteLayout, { TypeNoteCard } from '../../layouts/note-layout';
 
 export default function Note() {
   const router = useRouter();
   const { pid } = router.query;
-  console.log(pid);
+  const url = '/api/card';
+  const [card, setCard] = useState<TypeNoteCard | null>(null);
+
+  useEffect(() => {
+    const getCard = async () => {
+      if (pid) {
+        try {
+          const response = await fetch(`${url}/${pid}`);
+
+          if (response.ok) {
+            const result = await response.json();
+            setCard(result.card);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+
+    getCard();
+  }, [pid]);
 
   return (
     <Content>
-      <div>Note</div>
+      <NoteLayout card={card} />
     </Content>
   );
 }
