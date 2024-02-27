a089ab17361fbea0346613001b83072f657f7fb7
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-02-27 18:25:36 +0300

    fix: _app refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
 import { AppWrapper } from '../context/app-context';
 import Metrika from '../components/metrika';
+
+import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
