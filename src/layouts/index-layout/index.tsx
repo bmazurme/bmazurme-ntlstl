c091ad04924212352e4c296d50f0aecb7e3e0330
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 21:37:34 +0300

    fix: index-layout

--- src/layouts/index-layout/index.tsx
+++ src/layouts/index-layout/index.tsx
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
 import Content from '../content';
-// import { Logo } from '../../components';
-import { Menu } from '../../components/menu';
 
-import withExtraClass from '../../hocs/with-extra-class';
-
-
-// import { links } from '../../mock-data/links';
-
-// import styles from '../../styles/home.module.css';
-import menuStyle from './index-layout.module.css';
+import style from './index-layout.module.css';
 import styles from './company-section.module.css';
-import { useTranslation } from 'react-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
 
-const IndexMenu = withExtraClass(menuStyle)(Menu);
-
-// className={styles.main}
 export default function Home({ data }: any) {
   const list = data?.companyList;
   const [item, setItem] = useState<any>(null);
@@ -28,24 +16,23 @@ export default function Home({ data }: any) {
     }
   }, [list]);
 
-
-
-
   return (
     <Content>
-      <div>
+      <div className={style.main}>
         <h3 className={styles.name}>{item?.name}</h3>
-              <h4 className={styles.position}>
-                {item?.position}
-                <span className={styles.date}>
-                  {item?.from}
-                  {' '}
-                  -
-                  {' '}
-                  {item?.to}
-                </span>
-              </h4>
+        <h4 className={styles.position}>
+          {item?.position}
+          <span className={styles.date}>
+            {`${item?.from} - ${item?.to}`}
+          </span>
+        </h4>
 
+        <Link href="/profile">
+          <h4 className={styles.position}>CV</h4>
+        </Link>
+        <Link href="/portfolio">
+          <h4 className={styles.position}>Portfolio</h4>
+        </Link>
       </div>
     </Content>
   );
